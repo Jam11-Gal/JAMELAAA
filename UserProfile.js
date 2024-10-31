6d904f8e62febb79a24cc59fb32da234c3388b0f
Jamela <Jamelagalinato8@gmail.com>
2024-10-31 22:43:29 +0800

    CS2D_MTExam_GALINATO

---
+++ UserProfile.js
@@ -0,0 +1,17 @@
+// UserProfile.js
+
+// Collect user information
+let firstName = prompt("Jamela: ");
+let lastName = prompt("Galinato: ");
+let age = prompt("19: ");
+let favoriteNumber = prompt("11: ");
+let favoriteColor = prompt("Purple: ");
+
+// Calculate full name
+let fullName = firstName + " " + lastName;
+
+// Logging the information to the console
+console.log("fullname:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
---
+++ classSubjects.js
@@ -0,0 +1,11 @@
+// classSubjects.js
+
+// User Information
+const subjectTitle = "Art Appreciation";
+const classSchedule = "Mon/Fri 11:00AM - 12:30PM";
+const classroom = "C121";
+const classInstructor = "Mr.Marquez";
+const studentName = "Jamela Galinato";
+
+// Log user information to the console
+console.log(`(${Jamela Galinato}) is currently enrolled in (${Art Appreciation}) with a class schedule of (${Mon/Fri 11:00AM - 12:30PM}) at room (${C121}). The instructor for the subject is (${Mr.Marquez})`);
